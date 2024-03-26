2ba77825a7195afe364e4f191355a0b3d0530f7e
chriskayomb <ibond.chris@gmail.com>
2024-03-26 19:27:45 +0100

    Spring Data JPA with Spring Boot

--- src/main/java/com/example/springbootdemo/controller/EmployeeController.java
+++ src/main/java/com/example/springbootdemo/controller/EmployeeController.java
@@ -3,6 +3,7 @@ package com.example.springbootdemo.controller;
 import com.example.springbootdemo.model.Employee;
 import com.example.springbootdemo.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -10,6 +11,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/v1/employees")
 public class EmployeeController {
+    @Qualifier("employeeServiceImpl")
     @Autowired
     private EmployeeService employeeService;
 
@@ -30,4 +32,5 @@ public class EmployeeController {
     public String deleteEmployeeById(@PathVariable String id){
         return employeeService.deleteEmployeeById(id);
     }
+
 }
